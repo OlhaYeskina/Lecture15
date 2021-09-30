fc9724f0b03dda13e83f1526bef46dd8c6b9f32b
dinosaur321 <dinosaur321@users.com>
2021-09-30 21:53:23 +0300

    Create .json file
    Create .json file
    Create class User

--- src/main/java/RestfulApp.java
+++ src/main/java/RestfulApp.java
@@ -1,3 +1,6 @@
+
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -7,8 +10,24 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 
+
+/**
+ * 1. Необходимо сначала используя пакет java.net запросить имеющиеся в продаже билеты.
+ * Сохранить полученный xml в файл tickets.xml. Создать класс Ticket со всеми необходимыми полями.
+ * Считать файл tickets.xml и сохранить его как List<Ticket>. Из полученной коллекции выбрать все
+ * фильмы с категорией STANDART и сохранить в json формате в файл tickets.json.
+ *
+ * 2. Необходимо для userId = 1 забронировать билет с id = 1
+ *
+ * 3. Unmarshall tickets.json файл в список Ticket и записать его в Excel файл
+ */
+
 public class RestfulApp {
+    //private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+
+
         HttpClient client = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .connectTimeout(Duration.ofSeconds(60))
@@ -32,14 +51,25 @@ public class RestfulApp {
         
         
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://api.twitter.com/labs/2/tweets/1138505981460193280?expansions=attachments.media_keys&tweet.fields=created_at,author_id,lang,source,public_metrics,context_annotations,entities"))
+                .uri(new URI("https://reqres.in/api/users?page=2"))
                 .GET()
-                .setHeader("Accept", "*/*")
-                .setHeader("Content-Type", "application/json")
-                .setHeader("Authorization", String.format("Bearer %s", "authToken"))
+//                .setHeader("Accept", "*/*")
+//                .setHeader("Content-Type", "application/json")
+//                .setHeader("Authorization", String.format("Bearer %s", "authToken"))
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    //    String json = GSON.toJson(response);
+        System.out.println(response.body());
+        try (FileWriter file = new FileWriter("users.json")) {
+            //We can write any JSONArray or JSONObject instance to the file
+            file.write(response.body());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println();
     }
 }
---
+++ src/main/java/User.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class User {
+
+
+    private String name;
+    private String job;
+    List<User> list;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+}
